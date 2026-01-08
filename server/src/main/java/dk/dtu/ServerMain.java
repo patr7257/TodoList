0f6345e6eb8600a2f12623e731c29591462f47bc
s194870 <s194870@student.dtu.dk>
2026-01-08 12:06:26 +0000

    Resolve "feat: mob programming some server communication"

--- server/src/main/java/dk/dtu/ServerMain.java
+++ server/src/main/java/dk/dtu/ServerMain.java
@@ -2,7 +2,6 @@ package dk.dtu;
 
 import org.jspace.*;
 
-
 /**
  * Main server application
  * Minimal version (for learning):
@@ -19,6 +18,15 @@ public class ServerMain {
             SequentialSpace todoLists = new SequentialSpace();
             repo.add("todoLists", todoLists);
 
+            // Shared counter space (single tuple: (count:int))
+            SequentialSpace counter = new SequentialSpace();
+            repo.add("counter", counter);
+            counter.put(0);
+
+            // Request
+            SequentialSpace requests = new SequentialSpace();
+            repo.add(TupleSpaces.REQUESTS, requests);
+
             // Hardcoded lists for testing frontend connection: tuples of (listId, listName)
             todoLists.put("l1", "Shopping");
             todoLists.put("l2", "School");
@@ -32,9 +40,16 @@ public class ServerMain {
             repo.addGate("tcp://" + host + ":" + port + "/?keep");
 
             String todoListsUri = "tcp://" + host + ":" + port + "/todoLists?keep";
+            String counterUri = "tcp://" + host + ":" + port + "/counter?keep";
+            String requestsUri = "tcp://" + host + ":" + port + "/" + TupleSpaces.REQUESTS + "?keep";
+
             System.out.println("Server started.");
             System.out.println("todoLists URI: " + todoListsUri);
+            System.out.println("counter URI: " + counterUri);
+            System.out.println("requests URI: " + requestsUri);
 
+            Thread requestLoop = new Thread(() -> handleRequests(requests), "request-loop");
+            requestLoop.start();
             // Keep the server process alive
             while (true) {
                 Thread.sleep(60_000);
@@ -46,4 +61,85 @@ public class ServerMain {
             e.printStackTrace();
         }
     }
+
+    private static void handleRequests(Space requests) {
+        try {
+            while (true) {
+                System.out.println("hej");
+                // Wait for a ping request: (cmd, requestId, a1, a2, a3, a4)
+                Object[] req = requests.get(
+                        new ActualField(TupleSpaces.CMD_PING),
+                        new FormalField(String.class),
+                        new FormalField(Object.class),
+                        new FormalField(Object.class),
+                        new FormalField(Object.class),
+                        new FormalField(Object.class));
+
+                String cmdType = (String) req[0];
+                switch (cmdType) {
+                    case "ping":
+                        System.out.println("ping");
+                        handlePingRequest(req);
+                        break;
+                    case "list_create":
+                        System.out.println("listc");
+                        handleListCreateRequest(req);
+                        break;
+                    case "task_add":
+                        handleTaskAddRequest(req);
+                        break;
+                    case "task_status":
+                        handleTaskStatusRequest(req);
+                        break;
+                    case "task_assign":
+                        handleTaskAssignRequest(req);
+                        break;
+                    case "lists_get":
+                        handleListsGetRequest(req);
+                        break;
+                    case "tasks_get":
+                        handleTasksGetRequest(req);
+                        break;
+                }
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            System.out.println("Request loop interrupted");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void handleTasksGetRequest(Object[] req) {
+        System.out.println("handleTasksGetRequest");
+
+    }
+
+    private static void handleListsGetRequest(Object[] req) {
+        System.out.println("handleListsGetRequest");
+
+    }
+
+    private static void handleTaskAssignRequest(Object[] req) {
+        System.out.println("handleTaskAssignRequest");
+
+    }
+
+    private static void handleTaskStatusRequest(Object[] req) {
+        System.out.println("handleTaskStatusRequest");
+    }
+
+    private static void handleTaskAddRequest(Object[] req) {
+        System.out.println("handleTaskAddRequest");
+
+    }
+
+    private static void handleListCreateRequest(Object[] req) {
+        System.out.println("handleListCreateRequest");
+    }
+
+    private static void handlePingRequest(Object[] req) {
+        System.out.println("handlePingRequest");
+    }
+
 }
